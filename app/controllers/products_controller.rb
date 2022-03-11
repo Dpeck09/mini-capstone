8d594c396f5c98b68fd1b6713e2a29fbdc518d85
Davis Peck <Dpeck1997@gmail.com>
2022-03-11 11:38:50 -0700

    adding the RESTful create to the products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,13 @@ class ProductsController < ApplicationController
    product = Product.find_by(id: params[:id])
    render json: product.as_json
   end
+
+  def create
+    product = Product.new(name: "couch", price: "8000", image_url: "https://i.pinimg.com/originals/04/ab/61/04ab61338cfad5b66fe4280544a13d69.jpg", description: "the nicest couch you have ever sat on")
+
+    product.save
+
+    render json: product.as_json
+  end
+
 end
