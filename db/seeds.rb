648722f6062dabedbcf7dcde3a953ffed4dbcc46
Davis Peck <Dpeck1997@gmail.com>
2022-03-16 10:36:42 -0700

    adding a seeds_dump to the products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create!([
+  {name: "tv", price: "450.0", image_url: "https://images.samsung.com/is/image/samsung/levant-uhd-tu7000-ua43tu7000uxtw-rperspectivetitangray-229856895?$720_576_PNG$", description: "a nice new flatscreen for your living room", quantity: 19},
+  {name: "conditioner", price: "9.0", image_url: "https://www.biolage.com/-/media/Project/Biolage/Biolagecom/Images/PDP/Primary-Images/Full-Density/884486233943_Biolage_Advanced_Full_Density_Conditioner_400ml_USA.jpg", description: "Hopefully will make your hair soft", quantity: 230},
+  {name: "desk", price: "200.0", image_url: "https://m.media-amazon.com/images/I/71vSznKJfsL._AC_SX466_.jpg", description: "an all wood desk to help you stress out with coding", quantity: 27},
+  {name: "couch", price: "8000.0", image_url: "https://i.pinimg.com/originals/04/ab/61/04ab61338cfad5b66fe4280544a13d69.jpg", description: "the nicest couch you have ever sat on", quantity: 10},
+  {name: "Phone", price: "650.0", image_url: "https://www.att.com/idpassets/sales/uf/deals/cell-phone-deals/839650-Animated-Multi-Tile-iPhone12-retina.png", description: "you can call whoever you want", quantity: 200}
+])
